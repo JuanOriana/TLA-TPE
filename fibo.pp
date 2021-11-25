9b2799a9a798f11b3bc065b2dde0bffe15d2fe06
Agustin Naso <anaso@itba.edu.ar>
2021-11-25 18:01:08 -0300

    simple fibo program

---
+++ fibo.pp
@@ -0,0 +1,25 @@
+main:
+    int i = 2;
+    int n;
+    int n1=0;
+    int n2=1;
+    int sum=0;
+
+    write "Escribe hasta que numero quieres probar la serie de Fibonacci: ";
+    read n;
+
+    if(n > 0) { write "0 "; }
+    if(n > 1) { write "1 "; }
+    if(n > 2) {
+        while (i<n)
+        {
+            sum=n1+n2;
+            n1=n2;
+            n2=sum;
+            i = i + 1;
+            write sum;
+            write " ";
+        }
+    }
+    write "\n";
+return 1;
