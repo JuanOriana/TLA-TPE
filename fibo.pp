d40a86db61235f403576bd6e543149b007f8f4ef
Juan <joriana@itba.edu.ar>
2021-11-26 19:39:56 -0300

    Added () to all programs

--- fibo.pp
+++ fibo.pp
@@ -5,11 +5,11 @@ main:
     int n2=1;
     int sum=0;
 
-    write "Escribe hasta que numero quieres probar la serie de Fibonacci: ";
-    read n;
+    write ("Escribe hasta que numero quieres probar la serie de Fibonacci: ");
+    read (n);
 
-    if(n > 0) { write "0 "; }
-    if(n > 1) { write "1 "; }
+    if(n > 0) { write(0); }
+    if(n > 1) { write(1); }
     if(n > 2) {
         while (i<n)
         {
@@ -17,9 +17,9 @@ main:
             n1=n2;
             n2=sum;
             i = i + 1;
-            write sum;
-            write " ";
+            write (sum);
+            write (" ");
         }
     }
-    write "\n";
+    write ("\n");
 return 1;
